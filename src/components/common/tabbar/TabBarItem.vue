2aac53024fb92acdce18b8120546c94e2c9ff5e5
lixin2 <345064991@qq.com>
2020-01-03 16:54:46 +0800

    '2020-01-03修改'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+    data() {
+      return {
+        // isActive: true
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        //加入push和replace方法会返回一个promise, 你可能在控制台看到未捕获的异常
+        //在调用方法的时候用catch捕获异常
+        this.$router.push(this.path).catch(err => {
+          console.log('all good')
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
