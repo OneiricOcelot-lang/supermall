4d77ec87f3d230caeec96ec26daaf32a1127250f
lixin2 <345064991@qq.com>
2020-01-03 17:20:27 +0800

    '首页导航'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="home">
-    <nav-bar></nav-bar>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
@@ -16,5 +18,8 @@
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
